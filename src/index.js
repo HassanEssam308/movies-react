8d7dd364359fdf1d59643a7f63cfde066cd1d61f
Hassan <hassanesam308@gmail.com>
2023-05-16 09:21:26 +0300

    HashRouter

--- src/index.js
+++ src/index.js
@@ -16,11 +16,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <BrowserRouter> */}
+    
     <HashRouter>
     <Provider store={store} >
     <App />
     </Provider>
     </HashRouter>
+
     {/* </BrowserRouter> */}
 
   </React.StrictMode>
